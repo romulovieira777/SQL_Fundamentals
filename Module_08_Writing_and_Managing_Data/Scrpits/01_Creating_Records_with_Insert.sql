6237308474058550a7acc20a717000c315694f41
Romulo Vieira <romulo.vieira777@gmail.com>
2024-02-27 10:17:38 -0300

    Writing and Managing Data

---
+++ Module_08_Writing_and_Managing_Data/Scrpits/01_Creating_Records_with_Insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO
+    ATTENDEE (FIRST_NAME, LAST_NAME)
+VALUES
+    ('Thomas', 'Nield');
+
+
+SELECT
+    *
+FROM
+    ATTENDEE;
+
+
+INSERT INTO
+    ATTENDEE (FIRST_NAME, LAST_NAME, PHONE, EMAIL, VIP)
+VALUES
+    ('Jon', 'Skeeter', 4802185842, 'john.skeeter@rex.net', 1),
+    ('Sam', 'Scala', 2156783401, 'sam.scala@gmail.com', 0),
+    ('Brittany', 'Fisher', 5932857296, 'brittany.fisher@outlook.com', 0);
+
+
+SELECT
+    *
+FROM
+    ATTENDEE;
---
+++ Module_08_Writing_and_Managing_Data/Scrpits/02_Testing_Foreign_Key_Constraints.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+    COMPANY (NAME, DESCRIPTION,PRIMARY_CONTACT_ID)
+VALUES
+    ('RexApp Solutions', 'A mobile app delivery service', 3);
+
+
+SELECT
+    *
+FROM
+    COMPANY;
